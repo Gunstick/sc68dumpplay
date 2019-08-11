e99a906cc9de9c25000171e28431dc2689145dc9
Georges <gunstick@syn2cat.lu>
2019-08-12 00:16:01 +0200

    now plays, but not correctly.

--- sc68dply.s
+++ sc68dply.s
@@ -32,11 +32,29 @@ playloop:
     jsr a3printa4
     jsr print        ; print only a cr to stay on same line
     dc.b 13,0
-    adda.l #60,a6
-    cmpa.l #enddump,a6
-
-    blt.s playloop
+    ; let's see to play registers
+    adda.l #18,a6    ; this points now to R0
+    moveq #0,d3
+regloop:      ; play all 13 values pointed to by a6
+    cmp.b #'.',(a6)    ; no data, skip
+    beq.s skipreg
+    bsr a6hexchar2bind0
+    move.b d3,$ffff8800.w   ; select register
+    move.b d0,$ffff8802.w   ; write value
+    lea 1(a6),a6
+nextreg:
+    addq.b #1,d3          ; next register number
+    cmp.b #13,d3          ; we loop 14 times (0 to 13)
+    ble.s regloop
+    bra.s regloopend
+skipreg:
+    lea 3(a6),a6    ; we read a - so we skip this register
+    bra.s nextreg
+regloopend:
  
+    cmpa.l #enddump,a6    ; are we at end of dump
+    ble.s playloop
+
 ;   move.l #sc68dump,-(sp) ; address of text to print
 ;   move.w #9,-(sp)        ; gemdos cconws 
 ;   trap #1                ; call gemdos
@@ -60,14 +78,65 @@ initialize:                     ; go into super user mode
    trap #1     
    addq.l #6,a7   
    move.l d0,oldstack
+   ; set PSG into defined state
+psginit2:
+        moveq   #15,d0
+        lea     $ffff8800.w,a0
+        lea     psginittab,a1
+nextinit2:
+        move.b  (a1)+,(a0)
+        move.b  (a1)+,2(a0)
+        dbf     d0,nextinit2
+        rts
+
+
    rts
+psginittab:dc.b 0,$ff,1,$ff,2,$ff,3,$ff,4,$ff,5,$ff,6,0
+        dc.b 7,%11111011,8,0,9,0,10,0,11,5,12,100,13,2,$ff,0
+        even
    
 restore:                        ; go back into user mode
+   move.w (sp)+,sr
    move.l oldstack,-(a7)
    move.w #32,-(a7)
    trap #1     
    addq.l #6,a7
    rts
+a6hexchar2bind0:
+  ; converts the hexadecimal 2 characters at a0 to d0 binary byte
+    move.b (a6)+,d1
+    bsr tobin        ; convert lower D0 to bin nibble
+    move.b d1,d0     ; save value
+    move.b (a6)+,d1
+    bsr tobin
+    lsl #4,d1
+    add.b d1,d0
+    rts 
+tobin:
+    cmp.b #'A',d1    ; check if A-F
+    blt nochar
+    sub.b #7,d1      ; move chars close to nums
+nochar:
+    sub.b #$30,d1    ; convert binary
+    rts
+
+    bra.s hexdone
+nothex:
+    sub.b #'0',d0    ; convert ascii '0'-'9' to binary
+hexdone:
+    blt.s novalue    ; unable to convert
+    cmp.w #30*256,d0 ; if bigger than "9"x, substract for hex
+    ble.s nothex_
+    sub.w #'A'*256,d0  ; convert ascii 'A'x-'F'x to binary
+    bra.s hexdone_
+nothex_:
+    sub.w #'0'*256,d0    ; convert ascii '0'-'9' to binary
+hexdone_:
+    blt.s novalue    ; unable to convert
+    nop
+novalue: 
+    rts
+
 a3printa4:
   ; prints chars from a3 (included) to a4 (excluded)
   ; on return a3=a4
